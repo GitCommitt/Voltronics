5bb56c95e732c027cf4d93ab28552eba12fe630c
Daan Pronk <daanpronk570@gmail.com>
2025-06-10 19:57:21 +0200

    Modal added

--- Code/js/main.js
+++ Code/js/main.js
@@ -116,3 +116,18 @@ window.addEventListener('DOMContentLoaded', () => {
     checkbox && (checkbox.checked = false);
   }
 });
+
+    function openModal() {
+      document.getElementById('productModal').style.display = "block";
+    }
+    
+    function closeModal() {
+      document.getElementById('productModal').style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+      const modal = document.getElementById('productModal');
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
